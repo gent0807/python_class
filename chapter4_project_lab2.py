b16d89ffbc5647293aae27c53ebe3d16aa62e0e8
gent0807 <theloopholesnk@gmail.com>
2022-03-29 09:04:12 +0900

    good

--- chapter4_project_lab2.py
+++ chapter4_project_lab2.py
@@ -3,11 +3,36 @@ menu = '''Coffee menu!
         2.카페라테      2500
         3.카푸치노      3000
         4.캐러멜마끼아또 4000
-        0. 주문 종료
+        0.주문 종료
     종류?'''
 
 print('환영합니다. 음료를 선택하세요.')
 total = 0
 while True:
     order=int(input(menu))
-    if
+    if order ==0:
+        print()
+        print('주문 종료'.center(18,'*'))
+        break
+
+    else:
+        cnt= int(input('수량?'))
+        if order==1:
+            total+=cnt * 2000
+            print('\n%s %d개 주문'%('아메리카노', cnt))
+        elif order==2:
+            total+=cnt * 2500
+            print('\n%s %d개 주문'%('카페라테', cnt))
+        elif order==3:
+            total+=cnt * 3000
+            print('\n%s %d개 주문'%('카푸치노', cnt))
+        elif order==4:
+            total+=cnt * 4000
+            print('\n%s %d개 주문'%('캐퍼멜마끼아또', cnt))
+        else:
+            print('모르겠어요.')
+
+    print('현재 주문 가격: %d'%total)
+    print()
+print('총 주문 가격:%d'%total)
+print('안녕'.center(18,'*'))
