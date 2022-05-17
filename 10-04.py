ace545ce900b19f98ba527a4c82f1dd5eb6b6a3c
gent0807 <theloopholesnk@gmail.com>
2022-05-17 11:28:23 +0900

    ㅇㅇㅇ

---
+++ 10-04.py
@@ -0,0 +1,20 @@
+from tkinter import *
+def add():
+    n=int(count.get())
+    n+=1
+    count.set(n)
+def sub():
+    n=int(count.get())
+    n-=1
+    count.set(n)
+str=['더하기', '빼기']
+win=Tk()
+count=StringVar(value='0')
+
+data=Label(win, width=20, textvariable=count)
+data.grid(row=0, column=0, columnspan=2)
+
+badd=Button(win, text=str[0], command=add)
+badd.grid(row=1, column=0)
+badd=Button(win, text=str[1], command=sub)
+badd.grid(row=1, column=1)
